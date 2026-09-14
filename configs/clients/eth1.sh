62d839c3b1aff9cf09bf93d090327ddfa254f6da
wrliao <wrliao@gmail.com>
2026-09-14 16:51:32 +1000

    update eth1.sh

--- configs/clients/eth1.sh
+++ configs/clients/eth1.sh
@@ -3,8 +3,10 @@ ip link add link eth1 name eth1.10 type vlan id 10
 ip link add link eth1 name eth1.20 type vlan id 20
 ip link add link eth1 name eth1.30 type vlan id 30
 ip link add link eth1 name eth1.40 type vlan id 40
+ip link add link eth1 name eth1.50 type vlan id 50
 ip link set dev eth1.1 up
 ip link set dev eth1.10 up
 ip link set dev eth1.20 up
 ip link set dev eth1.30 up
 ip link set dev eth1.40 up
+ip link set dev eth1.50 up
